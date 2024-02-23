11f3af88a9943ca3fe77367a15be89d300d4ef2e
naiomi <Naiomi.Naidoo@icloud.com>
2024-02-23 16:07:03 +0100

    Implementing producer

--- commands/2-kafka-console-consumer.sh
+++ commands/2-kafka-console-consumer.sh
@@ -4,13 +4,13 @@
 kafka-console-consumer.sh 
 
 # consuming
-kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic first_topic
+kafka-console-consumer --bootstrap-server localhost:9092 --topic first_topic
 
 # other terminal
 kafka-console-producer.sh --bootstrap-server localhost:9092 --topic first_topic
 
 # consuming from beginning
-kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic first_topic --from-beginning
+kafka-console-consumer --bootstrap-server localhost:9092 --topic first_topic --from-beginning
 
 # display key, values and timestamp in consumer
 kafka-console-consumer --bootstrap-server localhost:9092 --topic first_topic --formatter kafka.tools.DefaultMessageFormatter --property print.timestamp=true --property print.key=true --property print.value=true --from-beginning
--- kafka-basics/src/main/java/com/naiomi/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/com/naiomi/kafka/ProducerDemo.java
@@ -1,7 +1,38 @@
 package com.naiomi.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
 public class ProducerDemo {
+
+private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class);
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        log.info ("Hello World");
+
+        //Create producer properties
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        //Create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        //Create a producer record
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "Hello World");
+
+        //Send data
+        producer.send(record);
+
+        //Flush and close producer
+        producer.flush();
+
+        //Close producer
+        producer.close();
     }
 }
